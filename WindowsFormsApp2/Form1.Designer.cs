4b3c5853262bd7708830159ad0772d02a217115a
Nikita Lysov <nlof142@gmail.com>
2021-05-18 23:39:31 +0700

    vozmognoPrav

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -29,60 +29,51 @@ namespace WindowsFormsApp2
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title7 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.NumberOfExper = new System.Windows.Forms.NumericUpDown();
             this.label6 = new System.Windows.Forms.Label();
             this.Start = new System.Windows.Forms.Button();
             this.Status = new System.Windows.Forms.Label();
             this.RelAver = new System.Windows.Forms.Label();
             this.RelVariance = new System.Windows.Forms.Label();
             this.ResChi = new System.Windows.Forms.Label();
-            this.Pprob = new System.Windows.Forms.NumericUpDown();
+            this.MeanValue = new System.Windows.Forms.NumericUpDown();
+            this.DispersValue = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NumberOfExper = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.NumberOfExper)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Pprob)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MeanValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DispersValue)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea7.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea7);
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            chartArea1.AxisX.ScaleBreakStyle.BreakLineStyle = System.Windows.Forms.DataVisualization.Charting.BreakLineStyle.None;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
             this.chart1.Location = new System.Drawing.Point(252, 33);
             this.chart1.Name = "chart1";
-            series7.ChartArea = "ChartArea1";
-            series7.IsValueShownAsLabel = true;
-            series7.Name = "Series1";
-            this.chart1.Series.Add(series7);
+            series1.ChartArea = "ChartArea1";
+            series1.IsValueShownAsLabel = true;
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(536, 405);
             this.chart1.TabIndex = 0;
-            title7.Alignment = System.Drawing.ContentAlignment.TopLeft;
-            title7.BorderWidth = 10;
-            title7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold);
-            title7.Name = "Title1";
-            title7.Text = "Probability";
-            this.chart1.Titles.Add(title7);
-            // 
-            // NumberOfExper
-            // 
-            this.NumberOfExper.Location = new System.Drawing.Point(5, 316);
-            this.NumberOfExper.Maximum = new decimal(new int[] {
-            -1530494977,
-            232830,
-            0,
-            0});
-            this.NumberOfExper.Name = "NumberOfExper";
-            this.NumberOfExper.Size = new System.Drawing.Size(120, 22);
-            this.NumberOfExper.TabIndex = 5;
-            this.NumberOfExper.ThousandsSeparator = true;
-            this.NumberOfExper.Value = new decimal(new int[] {
-            1000,
-            0,
-            0,
-            0});
+            title1.Alignment = System.Drawing.ContentAlignment.TopLeft;
+            title1.BorderWidth = 10;
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold);
+            title1.Name = "Title1";
+            title1.Text = "Probability";
+            this.chart1.Titles.Add(title1);
             // 
             // label6
             // 
@@ -135,58 +126,106 @@ namespace WindowsFormsApp2
             this.ResChi.Size = new System.Drawing.Size(0, 17);
             this.ResChi.TabIndex = 18;
             // 
-            // Pprob
+            // MeanValue
             // 
-            this.Pprob.DecimalPlaces = 2;
-            this.Pprob.Increment = new decimal(new int[] {
-            1,
+            this.MeanValue.DecimalPlaces = 2;
+            this.MeanValue.Location = new System.Drawing.Point(126, 73);
+            this.MeanValue.Maximum = new decimal(new int[] {
+            1410065407,
+            2,
             0,
+            0});
+            this.MeanValue.Minimum = new decimal(new int[] {
+            1410065407,
+            2,
             0,
-            131072});
-            this.Pprob.Location = new System.Drawing.Point(76, 127);
-            this.Pprob.Maximum = new decimal(new int[] {
-            1,
+            -2147483648});
+            this.MeanValue.Name = "MeanValue";
+            this.MeanValue.Size = new System.Drawing.Size(120, 22);
+            this.MeanValue.TabIndex = 19;
+            this.MeanValue.Value = new decimal(new int[] {
+            2,
             0,
             0,
             0});
-            this.Pprob.Name = "Pprob";
-            this.Pprob.Size = new System.Drawing.Size(114, 22);
-            this.Pprob.TabIndex = 19;
-            this.Pprob.Value = new decimal(new int[] {
-            6,
+            // 
+            // DispersValue
+            // 
+            this.DispersValue.DecimalPlaces = 2;
+            this.DispersValue.Location = new System.Drawing.Point(126, 126);
+            this.DispersValue.Maximum = new decimal(new int[] {
+            1410065407,
+            2,
+            0,
+            0});
+            this.DispersValue.Minimum = new decimal(new int[] {
+            1410065407,
+            2,
+            0,
+            -2147483648});
+            this.DispersValue.Name = "DispersValue";
+            this.DispersValue.Size = new System.Drawing.Size(120, 22);
+            this.DispersValue.TabIndex = 20;
+            this.DispersValue.Value = new decimal(new int[] {
+            3,
             0,
             0,
-            65536});
+            0});
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(5, 132);
+            this.label1.Location = new System.Drawing.Point(15, 77);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.Size = new System.Drawing.Size(43, 17);
             this.label1.TabIndex = 21;
-            this.label1.Text = "P0";
+            this.label1.Text = "Mean";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Despersia";
+            // 
+            // NumberOfExper
+            // 
+            this.NumberOfExper.FormattingEnabled = true;
+            this.NumberOfExper.Items.AddRange(new object[] {
+            "10",
+            "100",
+            "1000",
+            "10000"});
+            this.NumberOfExper.Location = new System.Drawing.Point(5, 328);
+            this.NumberOfExper.Name = "NumberOfExper";
+            this.NumberOfExper.Size = new System.Drawing.Size(121, 24);
+            this.NumberOfExper.TabIndex = 23;
+            this.NumberOfExper.Text = "10";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 514);
+            this.Controls.Add(this.NumberOfExper);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.Pprob);
+            this.Controls.Add(this.DispersValue);
+            this.Controls.Add(this.MeanValue);
             this.Controls.Add(this.ResChi);
             this.Controls.Add(this.RelVariance);
             this.Controls.Add(this.RelAver);
             this.Controls.Add(this.Status);
             this.Controls.Add(this.Start);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.NumberOfExper);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.NumberOfExper)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Pprob)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MeanValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DispersValue)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,15 +234,17 @@ namespace WindowsFormsApp2
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.NumericUpDown NumberOfExper;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button Start;
         private System.Windows.Forms.Label Status;
         private System.Windows.Forms.Label RelAver;
         private System.Windows.Forms.Label RelVariance;
         private System.Windows.Forms.Label ResChi;
-        private System.Windows.Forms.NumericUpDown Pprob;
+        private System.Windows.Forms.NumericUpDown MeanValue;
+        private System.Windows.Forms.NumericUpDown DispersValue;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox NumberOfExper;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -12,85 +12,123 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
-        const int numOfNums = 5;
-        float[] prob = new float[numOfNums];
-        float p0;
+        float[] values;
+        float me;
+        float de;
         int n;
-        float krit = 9.488f;
+        float[] krit = {7.8145f,14.057f,18.307f,22.362f };
+        int stage;
 
         public Form1()
         {
             InitializeComponent();
 
         }
-        void InitProbGeom()
-        {
-            float sum = 0;
-            for (int i = 0; i < numOfNums - 1; i++)
-            {
-                prob[i] = (float)(p0 * Math.Pow((1 - p0), i));
-                //Console.WriteLine(p * Math.Pow((1 - p), i));
-                sum += prob[i];
-                //Console.WriteLine(sum);
-            }
-            prob[numOfNums - 1] = 1 - sum;
-        }
+
         private void Start_Click(object sender, EventArgs e)
         {
             chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
             Random rnd = new Random();
-            p0 = (float)Pprob.Value;
-            InitProbGeom();
+            me = (float)MeanValue.Value;
+            de = (float)DispersValue.Value;
 
-            float q;
-            
-            n = (int)NumberOfExper.Value;
-            int[] mass = new int[numOfNums];
+            n = Convert.ToInt32(NumberOfExper.Text);
+            stage = NumberOfExper.SelectedIndex;
+            values = new float[n];
+            float me2 = 0;
+            float de2 = 0;
             for (int i = 0; i< n; i++)
             {
-                int x = (int)(Math.Log(rnd.NextDouble()) / Math.Log(1 - p0));
-                if (x > prob.Length - 1) { mass[prob.Length - 1]++; }
-                else
-                {
-                    mass[x]++;
-                }
-            }
-            float[] prob1 = new float[mass.Length];
-            float chiSquare = 0;
-            for(int i = 0; i< mass.Length; i++)
-            {
-                prob1[i] = (float)mass[i] / n;
-                //chiSquare += (float)Math.Pow(prob[i]*n-prob1[i]*n,2)/(prob1[i]*n);
-                chiSquare += (float)Math.Pow(mass[i],2)/(prob[i]*n);
-                Console.WriteLine(chiSquare);
-                chart1.Series[0].Points.AddXY(i+1, (float)mass[i] / n);
-            }
-            chiSquare -= n;
-            Console.WriteLine(chiSquare);
+                double a1 = rnd.NextDouble();
+                double a2 = rnd.NextDouble();
+                double kor = Math.Sqrt(-2 * Math.Log(a1));
+                double cos = Math.Cos(2 * Math.PI * a2);
+                double sin = Math.Sin(2 * Math.PI * a2);
+                float x = (float)(kor * cos * Math.Sqrt(de) + me);
+                values[i] = x;
+                me2 += x;
+                de2 += (float)Math.Pow(x, 2);
+                //x = kor * sin * Math.Sqrt(de) + me;
 
-            float me1 = 0;
-            float me2 = 0;
-            float de1 = 0;
-            float de2 = 0;
-            for(int i = 0; i< prob1.Length; i++)
-            {
-                me1 += (i + 1) * prob[i];
-                me2 += (i + 1) * prob1[i];
-                de1 += (float)Math.Pow((i + 1),2) * prob[i];
-                de2 += (float)Math.Pow((i + 1),2) * prob1[i];
             }
-            de1 -= (float)Math.Pow(me1, 2);
+            me2 /= n;
+            de2 /= n;
             de2 -= (float)Math.Pow(me2, 2);
-            RelAver.Text = $"{me2} (error = {Math.Round(Math.Abs(me2-me1)/Math.Abs(me2)*100)}%)";
-            RelVariance.Text = $"{de2} (errors = {Math.Round(Math.Abs(de2 - de1) / Math.Abs(de2)*100,2)}%)";
-            if ( chiSquare < krit )
+            Stats(n, me2, de2);
+        }
+
+
+
+        void Stats(int numbers, float me2, float de2)
+        {
+            double min, max;
+            min = max = values[0];
+            for (int i = 0; i < values.Length; i++)
             {
-                ResChi.Text = $"{chiSquare} < {krit}  is {false}";
+                if (values[i] < min) min = values[i];
+                if (values[i] > max) max = values[i];
+            }
+            double smallKoef = (max - min) * 0.05;
+            double interval = (max - min) + smallKoef;
+            
+            int k = (int)Math.Round( Math.Log(numbers)/Math.Log(2) + 1);
+            double step = interval / k;
+
+            double q = min - smallKoef / 2;
+            int[] stat = new int[k];
+            float chi = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                stat[i] = Check(q, q + step);
+                float p = (float)(q + step - q) * Density( (float)(q + step + q) / 2);
+                chi += (float)Math.Pow(stat[i], 2) / (numbers * p);
+                chart1.Series[1].Points.AddXY(q,Density((float)(q + step + q) / 2));
+                chart1.Series[0].Points.AddXY(q, (double)stat[i]/numbers);
+                q += step;
+            }
+
+            chi -= numbers;
+            SetStatInfo(chi,me2,de2);
+
+            foreach (var t in stat)
+            {
+                Console.WriteLine(t);
+
+            }
+            Console.WriteLine(chi);
+            Console.WriteLine("--------------" + k + "------------------");
+        }
+
+
+        void SetStatInfo(float chi, float me2, float de2)
+        {
+            if (chi < krit[stage])
+            {
+                ResChi.Text = $"{chi} < {krit[stage]}  is {false}";
             }
             else
             {
-                ResChi.Text = $"{chiSquare} > {krit}  is {true}";
+                ResChi.Text = $"{chi} > {krit[stage]}  is {true}";
             }
+            RelAver.Text = $"{me2} (error = {Math.Round(Math.Abs(me2 - me) / Math.Abs(me2) * 100)}%)";
+            RelVariance.Text = $"{de2} (errors = {Math.Round(Math.Abs(de2 - de) / Math.Abs(de2) * 100, 2)}%)";
+        }
+
+        int Check(double a, double b )
+        {
+            int count = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] >= a && values[i] < b) count++;
+            }
+            return count;
+        }
+        float Density( float x)
+        {
+            float value = (float)Math.Pow(Math.E,Math.Pow((x - me),2)/(-2*Math.Pow(de,2)));
+            return (float)value/ (float)(de * Math.Sqrt(2*Math.PI));
         }
     }
 }
